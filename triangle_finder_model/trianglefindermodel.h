74ae0b605dc1f7586d475b3e32a6df909b4cfb44
Joris Nonnast <djjorisdj@gmail.com>
2019-11-05 22:29:19 +0100

    Switched from qt to cmake

--- triangle_finder_model/trianglefindermodel.h
+++ triangle_finder_model/trianglefindermodel.h
@@ -6,9 +6,9 @@
 #include <string>
 
 #include "triangleinfocontainer.h"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 class TriangleFinderModel
 {
